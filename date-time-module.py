8036a7a3f6a4a7617ad04541ac1ae63eb6b57d13
Amal Rehman <amalrehman8523690@gmail.com>
2026-09-01 04:41:59 +0500

    date time module

---
+++ date-time-module.py
@@ -0,0 +1,15 @@
+import datetime
+#current date time
+now = datetime.datetime.now()
+print("current date and time:" ,now)
+
+today = datetime.date
+print("today date:" , now)
+import time
+# wait for 3 sec
+print("program started")
+time.sleep(10)
+print("program finished") 
+
+
+
