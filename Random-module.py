58be0b87a940a2ed7e90a979fc96b44f7429714c
Amal Rehman <amalrehman8523690@gmail.com>
2026-09-01 04:43:49 +0500

    random module

---
+++ Random-module.py
@@ -0,0 +1,8 @@
+import random
+#random number
+print(random.randint(50,100))
+# Generate 6-digit otp
+print(random.randint(600000,999999))
+#random fruit 
+fruit = ["mango","orange","banna","apple","mulberry"]
+print(random.choice(fruit))
