9f42b8c8f1d29cb587610be37ec27d18015a18f2
Amal Rehman <amalrehman8523690@gmail.com>
2026-09-01 04:42:34 +0500

    Add files via upload

---
+++ Math-module.py
@@ -0,0 +1,13 @@
+import math
+#square root of 144
+print(math.sqrt(144))
+#power
+print(math.pow(7,4))
+#pi
+print(math.pi)
+#area oof circle
+radius=10
+area = math.pi*radius *radius
+print("arae of circle:" , area)
+
+
